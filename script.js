8b2221dd22a0bfa7197a7b915cd554d0482fb130
João Batista!! <jb0524056@gmail.com>
2026-01-21 17:58:38 -0300

    Correção de erros diversos de caminhos e adição da liturgia de 22.01.26

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "logos-fotos/menu-white36dp.svg";
+        document.querySelector('.icon').src = "logos-fotos/menu_white_36dp.svg";
     } else {
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "logos-fotos/close_white_36dp.svg";
